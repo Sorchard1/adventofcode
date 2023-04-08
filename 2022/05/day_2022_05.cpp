551f323948f6ca92d7f35d846122ff170861e33f
simon <simon.orchard1@btinternet.com>
2023-04-08 09:24:23 +0100

    Part 1 of day 5.

--- 2022/05/day_2022_05.cpp
+++ 2022/05/day_2022_05.cpp
@@ -8,11 +8,119 @@
 #include <vector>
 #include <cassert>
 #include <algorithm>
+#include <deque>
 
 namespace day202205 {
 
     int run(std::filesystem::path data_path)
     {
+
+        std::ifstream datafile;
+        datafile.open(data_path);
+        std::string myline;
+        bool header_loaded = false;
+
+        std::unordered_map<int, std::deque<std::string>> crate_stacks;
+        std::string letter;
+        std::vector<int> n_moving;
+        std::vector<int> move_from;
+        std::vector<int> move_to;
+        std::string new_string;
+        unsigned int split_index;
+
+        // Read the data file.
+        if (datafile.is_open()) {
+            while (datafile) {
+                std::getline(datafile, myline);
+                if (myline.length()!=0 and (not header_loaded)) {
+                    // When all the letters are sorted, the number 1 is entered
+                    if (myline.substr(1, 1) != "1"){
+                        // Read in the header
+                        // There are 4 characters per entry "[A] " except no gap on the last
+                        for (int i = 0; i<(myline.length() + 1)/4; i++){
+                            letter = myline[ i * 4 + 1];
+                            // if there is a letter in this column
+                            if (letter != " ") {
+                                // Make a new stack
+                                if (crate_stacks.find(i) != crate_stacks.end()){
+                                    crate_stacks.insert({{i}, {}});
+
+                                }
+                                // Add to existing stack
+                                crate_stacks[i].push_back(letter);
+
+                            }
+
+                        }
+                    }
+
+                }
+                else if (myline.length()==0 and (not header_loaded)){
+                    // Switch to reading in the moving parts
+                    header_loaded = true;
+                }
+                else if (myline.length()!=0) {
+                    // Reading in the moving parts
+                    std::cout << "Data " << myline << "\n";
+
+                    // Remove the string "move "
+                    myline = myline.erase(0, 5);
+
+                    // Find the next space, from 0 to this index is the number of items to move.
+                    split_index =  myline.find(" ") + 1;
+                    n_moving.push_back(std::stoi(myline.substr(0, split_index)));
+
+                    // Remove the string for the number of items and the space
+                    myline = myline.erase(0, split_index);
+
+                    // Remove the string "from "
+                    myline = myline.erase(0, 5);
+
+                    // Find the next space, from 0 to this index is the 'from' position.
+                    split_index =  myline.find(" ") + 1;
+                    move_from.push_back(std::stoi(myline.substr(0, split_index)) - 1);
+
+                    // Remove the string for the 'from' position and the space
+                    myline = myline.erase(0, split_index);
+
+                    // Remove the string "to " then add the number for 'to'
+                    myline = myline.erase(0, 3);
+                    move_to.push_back(std::stoi(myline) - 1);
+
+                }
+            }
+        }
+        else {
+            std::cout << "\n File not open!";
+        }
+
+        for (int j = 0; j < crate_stacks[0].size(); j++) {
+            std::cout << 0 + 1 << " " << crate_stacks[0][j] << "\n";
+        }
+        for (int j = 0; j < crate_stacks[1].size(); j++) {
+            std::cout << 1 + 1 << " " << crate_stacks[1][j] << "\n";
+        }
+        for (int j = 0; j < crate_stacks[2].size(); j++) {
+            std::cout << 2 + 1 << " " << crate_stacks[2][j] << "\n";
+        }
+
+        // Perform the moving
+        for (int i = 0; i < n_moving.size(); i++) {
+//            std::cout << n_moving[i] << " " << move_from[i] + 1 << " " << move_to[i] + 1 << "\n";
+            for (int j = 0; j < n_moving[i]; j++){
+                std::cout << crate_stacks[move_to[i]][0] << "\n";
+                std::cout << crate_stacks[move_from[i]][0] << "\n";
+                crate_stacks[move_to[i]].push_front(crate_stacks[move_from[i]][0]);
+                crate_stacks[move_from[i]].pop_front();
+
+            }
+        }
+
+        std::cout << "Part 1 ";
+        for (int i = 0; i < crate_stacks.size(); i++) {
+            std::cout << crate_stacks[i][0];
+        }
+        std::cout << "\n";
 
         return 0;
     }
