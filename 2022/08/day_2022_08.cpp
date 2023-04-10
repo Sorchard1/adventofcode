11b7ebed357f364a18f527a694b9db67ad7b97cc
simon <simon.orchard1@btinternet.com>
2023-04-10 15:56:19 +0100

    Day 8 part 2 complete.

--- 2022/08/day_2022_08.cpp
+++ 2022/08/day_2022_08.cpp
@@ -23,6 +23,8 @@ namespace day202208 {
 
         std::vector<std::vector<bool>> visible;
         std::vector<bool> visible_row;
+        std::vector<std::vector<int>> score;
+        std::vector<int> score_row;
 
         // Read the data file.
         if (datafile.is_open()) {
@@ -34,22 +36,19 @@ namespace day202208 {
                     for (char i : myline) {
                         tree_row.push_back(i - '0');
                         visible_row.push_back(false);
+                        score_row.push_back(1);
 
                     }
                     tree_heights.push_back(tree_row);
                     visible.push_back(visible_row);
+                    score.push_back(score_row);
                 }
             }
         }
         else {
             std::cout << "\n File not open!";
         }
-//        for (int i = 0; i < tree_heights.size(); i++) {
-//            for (int j = 0; j < tree_heights[i].size(); j++) {
-//                std::cout << tree_heights[i][j] << "\n";
-//
-//            }
-//        }
+
         std::vector<int> tallest_left;
         std::vector<int> tallest_top = tree_heights[0];
         std::vector<int> tallest_right;
@@ -96,6 +95,7 @@ namespace day202208 {
         int j_r;
 
         // Find the tallest tree after each tree
+        // TODO could skip lots of searching at edges
         for (int i = 1; i < col_length - 1; i++) {
             for (int j = 1; j < row_length - 1; j++) {
                 i_r = col_length - i - 1;
@@ -130,6 +130,67 @@ namespace day202208 {
         }
 
         std::cout << "Part 1 " << n_visible << "\n";
+        int diff;
+        int max_score = 0;
+
+        for (int i = 1; i < col_length - 1; i++) {
+            for (int j = 1; j < row_length - 1; j++) {
+                current_height = tree_heights[i][j];
+                std::cout << current_height << "\n";
+            }
+        }
+        // TODO we know edge = 0 so could ignore them to speed up.
+        for (int i = 1; i < col_length - 1; i++) {
+            for (int j = 1; j < row_length - 1; j++) {
+
+                // Go right
+                diff = 0;
+                current_height = tree_heights[i][j];
+                for (int j_row = j + 1; j_row < row_length; j_row++) {
+                    diff = j_row - j;
+                    if (tree_heights[i][j_row] >= current_height) {
+                        break;
+                    }
+                }
+                score[i][j] *= diff;
+                // Go down
+                diff = 0;
+                for (int i_row = i + 1; i_row < col_length; i_row++) {
+                    diff = i_row - i;
+                    if (tree_heights[i_row][j] >= current_height) {
+                        break;
+                    }
+                }
+                score[i][j] *= diff;
+
+                // Go left
+                diff = 0;
+                for (int j_row = j - 1; j_row > -1; j_row--) {
+                    diff = j - j_row;
+                    if (i == 2 and j == 3) {
+                        std::cout << "Height check " << i << " " << j << " " << j_row << " " << current_height << " " << tree_heights[i][j_row] << "\n";
+                    }
+                    if (tree_heights[i][j_row] >= current_height) {
+                        break;
+                    }
+                }
+                score[i][j] *= diff;
+                // Go up
+                diff = 0;
+                for (int i_row = i - 1; i_row > -1; i_row--) {
+                    diff = i - i_row;
+                    if (tree_heights[i_row][j] >= current_height) {
+                        break;
+                    }
+                }
+                score[i][j] *= diff;
+                std::cout << i << " " << j << " " << score[i][j] << "\n";
+                if (score[i][j]> max_score) {
+                    max_score = score[i][j];
+                }
+            }
+        }
+        std::cout << "Part 2 " << max_score << "\n";
 
         return 0;
     }
