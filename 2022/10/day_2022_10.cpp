d2eefe8e8cd812f8b118cf15e957b45cce15491b
simon <simon.orchard1@btinternet.com>
2023-04-11 13:01:05 +0100

    Day 10 part 1 completed.

--- 2022/10/day_2022_10.cpp
+++ 2022/10/day_2022_10.cpp
@@ -14,6 +14,53 @@ namespace day202210 {
     int run(std::filesystem::path data_path)
     {
 
+        std::ifstream datafile;
+        datafile.open(data_path);
+        std::string myline;
+
+        std::vector<int> registers;
+        int current_register = 1;
+        int split_index;
+        int counter = 0;
+
+        // Read the data file.
+        if (datafile.is_open()) {
+            while (datafile) {
+                std::getline(datafile, myline);
+                if (myline.length()!=0) {
+                    if (myline[0] == 'a') {
+                        // Extra push back for this command
+                        registers.push_back(current_register);
+                        counter += 1;
+
+                        // For adding strengths
+                        split_index =  myline.find(" ") + 1;
+                        current_register += std::stoi(myline.substr(split_index));
+
+                    }
+                    counter += 1;
+
+                    if (counter < 221) {
+                        std::cout << counter << " " << current_register << " " << myline << "\n";
+                    }
+                    registers.push_back(current_register);
+                }
+            }
+        }
+        else {
+            std::cout << "\n File not open!";
+        }
+
+        int strength = 0;
+        std::vector<int> cycles = {20, 60, 100, 140, 180, 220};
+        for (int cycle :cycles){
+            // THe -2 is -1 for indexing and -1 because cycle has not completed.
+            strength += cycle * registers[cycle - 2];
+            std::cout << cycle << " " << registers[cycle - 2] << "\n";
+
+        }
+
+        std::cout << "Part 1 " << strength << "\n";
         return 0;
     }
 }
