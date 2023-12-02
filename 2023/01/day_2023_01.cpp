42a4df8da4d082074e229a80f7e4d9588ccf4646
simon <simon.orchard@luffy.ai>
2023-12-02 12:22:09 +0000

    Added 2023 files properly and finished day 1 part 1.

--- 2023/01/day_2023_01.cpp
+++ 2023/01/day_2023_01.cpp
@@ -10,6 +10,45 @@
 namespace day202301 {
     int run(std::filesystem::path data_path)
     {
+
+        std::ifstream datafile;
+        datafile.open(data_path);
+        std::string myline;
+        int value;
+        std::string new_string;
+        std::vector<int> first_integer;
+        std::vector<int> last_integer;
+        unsigned int line_length;
+        int part_1_answer;
+        // Read the data file.
+        if (datafile.is_open()) {
+            while (datafile) {
+                std::getline(datafile, myline);
+                line_length = myline.length();
+                for (int i = 0; i<line_length; i++) {
+                    value = myline[i]-'0';
+                    if (value <= 9) {
+                        first_integer.push_back(value);
+                        break;
+
+                    }
+                }
+                // reversed loop from end of line
+                for (int i = 0; i<line_length; i++) {
+                    value = myline[line_length-i-1]-'0';
+                    if (value <= 9) {
+                        last_integer.push_back(value);
+                        break;
+                    }
+                }
+            }
+        }
+        // add together the numbers
+        for (int i = 0; i<first_integer.size(); i++) {
+            part_1_answer += 10*first_integer[i] + last_integer[i];
+        }
+
+        std::cout << "Answer to part 1 is: " << part_1_answer << "\n";
         return 0;
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,31 @@
 #include "day_2022_23.h"
 #include "day_2022_24.h"
 #include "day_2022_25.h"
+#include "day_2023_01.h"
+#include "day_2023_02.h"
+#include "day_2023_03.h"
+#include "day_2023_04.h"
+#include "day_2023_05.h"
+#include "day_2023_06.h"
+#include "day_2023_07.h"
+#include "day_2023_08.h"
+#include "day_2023_09.h"
+#include "day_2023_10.h"
+#include "day_2023_11.h"
+#include "day_2023_12.h"
+#include "day_2023_13.h"
+#include "day_2023_14.h"
+#include "day_2023_15.h"
+#include "day_2023_16.h"
+#include "day_2023_17.h"
+#include "day_2023_18.h"
+#include "day_2023_19.h"
+#include "day_2023_20.h"
+#include "day_2023_21.h"
+#include "day_2023_22.h"
+#include "day_2023_23.h"
+#include "day_2023_24.h"
+#include "day_2023_25.h"
 
 
 int main() {
@@ -228,6 +253,181 @@ int main() {
         {
             day_dir = "25";
             day202225::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202301:
+        {
+            day_dir = "01";
+            day202301::run(cwd / year_dir / day_dir / data_name);
+            break;
+
+        }
+        case 202302:
+        {
+            day_dir = "02";
+            day202302::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202303:
+        {
+            day_dir = "03";
+            day202303::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202304:
+        {
+            day_dir = "04";
+            day202304::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202305:
+        {
+            day_dir = "05";
+            day202305::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202306:
+        {
+            day_dir = "06";
+            day202306::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202307:
+        {
+            day_dir = "07";
+            day202307::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202308:
+        {
+            day_dir = "08";
+            day202308::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202309:
+        {
+            day_dir = "09";
+            day202309::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202310:
+        {
+            day_dir = "10";
+            day202310::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202311:
+        {
+            day_dir = "11";
+            day202311::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202312:
+        {
+            day_dir = "12";
+            day202312::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202313:
+        {
+            day_dir = "13";
+            day202313::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202314:
+        {
+            day_dir = "14";
+            day202314::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202315:
+        {
+            day_dir = "15";
+            day202315::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202316:
+        {
+            day_dir = "16";
+            day202316::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202317:
+        {
+            day_dir = "17";
+            day202317::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202318:
+        {
+            day_dir = "18";
+            day202318::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202319:
+        {
+            day_dir = "19";
+            day202319::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202320:
+        {
+            day_dir = "20";
+            day202320::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202321:
+        {
+            day_dir = "21";
+            day202321::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202322:
+        {
+            day_dir = "22";
+            day202322::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202323:
+        {
+            day_dir = "23";
+            day202323::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202324:
+        {
+            day_dir = "24";
+            day202324::run(cwd / year_dir / day_dir / data_name);
+
+            break;
+        }
+        case 202325:
+        {
+            day_dir = "25";
+            day202325::run(cwd / year_dir / day_dir / data_name);
 
             break;
         }
